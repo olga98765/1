6b71c7410ad7cca92fe55982f4a59daac138a048
ola <o.monko@o2.pl>
2024-03-05 18:24:07 +0100

    ex15 finished

--- ex15.java
+++ ex15.java
@@ -5,40 +5,32 @@ import java.util.Arrays;
 public class ex15 {
 
     public static void main(String[] args) {
-        int[] grades = { 5, 5, 1 };
+        int[] grades = { 1, 2, 3 };
         int sum = Arrays.stream(grades).sum();
         int average = sum / grades.length;
 
-        System.out.println(average);
-
         switch (average) {
             case 1:
-              System.out.println("Twoja ocena to F");
-              break;
+                System.out.println("Twoja ocena to F");
+                break;
             case 2:
-              System.out.println("Twoja ocena to E");
-              break;
+                System.out.println("Twoja ocena to E");
+                break;
             case 3:
-              System.out.println("Twoja ocena to D");
-              break;
+                System.out.println("Twoja ocena to D");
+                break;
             case 4:
-              System.out.println("Twoja ocena to C");
-              break;
+                System.out.println("Twoja ocena to C");
+                break;
             case 5:
-              System.out.println("Twoja ocena to B");
-              break;
+                System.out.println("Twoja ocena to B");
+                break;
             case 6:
-              System.out.println("Twoja ocena to A");
-              break;
+                System.out.println("Twoja ocena to A");
+                break;
             default:
-              System.out.println("Podaj prawidłowe oceny");
-          }
-
-
-
-
-
-
+                System.out.println("Podaj prawidłowe oceny");
+        }
 
     }
 
