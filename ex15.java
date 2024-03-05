ed656a47997e11fc0f5c874a05a459a1fc1b756e
ola <o.monko@o2.pl>
2024-03-05 18:15:01 +0100

    ex15 average

--- ex15.java
+++ ex15.java
@@ -1,10 +1,15 @@
 // 5) Napisz program, który dla tablicy wyników z kilkoma ocenami obliczy średnią ocen i określi odpowiedni stopień zaliczenia (np. "A", "B", "C" itp.).
 
+import java.util.Arrays;
+
 public class ex15 {
 
     public static void main(String[] args) {
-   
+        int[] grades = { 1, 1, 1 };
+        int sum = Arrays.stream(grades).sum();
+        int average = sum / grades.length;
 
-        System.out.println();
+        System.out.println(average);
     }
+
 }
