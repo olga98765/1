009680b63fe1376a9117495601d33a71c56d8da8
ola <o.monko@o2.pl>
2024-03-05 18:20:55 +0100

    switch added

--- ex15.java
+++ ex15.java
@@ -5,11 +5,41 @@ import java.util.Arrays;
 public class ex15 {
 
     public static void main(String[] args) {
-        int[] grades = { 1, 1, 1 };
+        int[] grades = { 5, 5, 1 };
         int sum = Arrays.stream(grades).sum();
         int average = sum / grades.length;
 
         System.out.println(average);
+
+        switch (average) {
+            case 1:
+              System.out.println("Twoja ocena to F");
+              break;
+            case 2:
+              System.out.println("Twoja ocena to E");
+              break;
+            case 3:
+              System.out.println("Twoja ocena to D");
+              break;
+            case 4:
+              System.out.println("Twoja ocena to C");
+              break;
+            case 5:
+              System.out.println("Twoja ocena to B");
+              break;
+            case 6:
+              System.out.println("Twoja ocena to A");
+              break;
+            default:
+              System.out.println("Podaj prawidłowe oceny");
+          }
+
+
+
+
+
+
+
     }
 
 }
