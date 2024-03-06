2d6d3721c233b3981522bb3a83c34b0ed7f73065
ola <o.monko@o2.pl>
2024-03-06 09:25:35 +0100

    if added

--- ex16.java
+++ ex16.java
@@ -5,9 +5,17 @@ public class ex16 {
 
     public static void main(String[] args) {
    
-String text = "palindrom";
+String text = "malindrom";
 char[] textToLetters = text.toCharArray();
 
         System.out.println(textToLetters[0]);
+if (textToLetters[0] == textToLetters[textToLetters.length-1]) {
+
+    System.out.println("palindrom");
+} else {
+        System.out.println("nie palindrom");
     }
+    
+}
+
 }
