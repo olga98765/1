67c693879a8ae033523cb6efe9156877e3c4b385
ola <o.monko@o2.pl>
2024-03-06 09:34:43 +0100

    for added

--- ex16.java
+++ ex16.java
@@ -3,15 +3,29 @@
 
 public class ex16 {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
    
 String text = "malindrom";
 char[] textToLetters = text.toCharArray();
 
         System.out.println(textToLetters[0]);
+
+
 if (textToLetters[0] == textToLetters[textToLetters.length-1]) {
 
+
+
+for(int i=0; i<textToLetters.length/2; i++) {
+System.out.println(i);
+}
+
+
     System.out.println("palindrom");
+
+
 } else {
         System.out.println("nie palindrom");
     }
