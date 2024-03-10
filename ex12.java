4d02dfd36aee0e19bc4f754dbcc0c3bff02be795
ola <o.monko@o2.pl>
2024-03-10 12:34:19 +0100

    ex12 completed

--- ex12.java
+++ ex12.java
@@ -1,11 +1,10 @@
 // 2) Stwórz tablicę liczb całkowitych a następnie oblicz średnią wartość.
 
-
 public class ex12 {
 
     public static void main(String[] args) {
-        
-        int[] numbers = {1, 2, 3, 4, 5, 6};
+
+        int[] numbers = { 1, 2, 3, 4, 5, 6 };
         int sum = 0;
         for (int number : numbers) {
             sum += number;
